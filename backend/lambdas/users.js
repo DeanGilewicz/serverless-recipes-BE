15ba3d73ebfb079407f31fae6de22437f5c8ee26
Dean Gilewicz <gilewiczd@gmail.com>
2019-11-04 22:29:46 -0700

    add confirm cognito user endpoint

--- backend/lambdas/users.js
+++ backend/lambdas/users.js
@@ -5,42 +5,8 @@ const documentClient = new AWS.DynamoDB.DocumentClient();
 
 module.exports.createUser = (event, context, callback) => {
 	const eventBodyJson = JSON.parse(event.body);
-  // const claims = event.requestContext.authorizer.claims;
-  // const username = claims['cognito:username'];
 
-	console.log("eventBodyJson", eventBodyJson);
-	// console.log("claims", claims);
-	// console.log("username: ", username);
-	console.log("context", context);
-
-	// const params = {
-  //   TableName: process.env.USER_TABLE_NAME,
-	// 	Item: {
-	// 		"userId": {
-	// 			S: userId
-	// 		},
-	// 		"firstName": {
-	// 			S: registrationJson.firstName
-	// 		},
-	// 		"lastName": {
-	// 			S: registrationJson.lastName
-	// 		},
-	// 		"streetAddress": {
-	// 			S: registrationJson.streetAddress
-	// 		},
-	// 		"city": {
-	// 			S: registrationJson.city
-	// 		},
-	// 		"state": {
-	// 			S: registrationJson.state
-	// 		},
-	// 		"zip": {
-	// 			S: registrationJson.zip
-	// 		}
-	// 	}
-  // };
-
-	var params = {
+	const params = {
 		ClientId: process.env.AWS_APP_CLIENT_ID,
 		Password: eventBodyJson.password,
 		Username: eventBodyJson.emailAddress,
@@ -62,6 +28,33 @@ module.exports.createUser = (event, context, callback) => {
 	};
 
 	cognitoIdentityServiceProvider.signUp(params, function(err, data) {
+		if( err ) {
+			console.error(err, err.stack);
+			return;
+		}
+		const response = {
+			statusCode: 200,
+			headers: {
+				"Access-Control-Allow-Origin": "*", 			// Required for CORS support to work
+				"Access-Control-Allow-Credentials": true	// Required for cookies, authorization headers with HTTPS
+			},
+			body: JSON.stringify(data),
+		};
+		callback(null, response);
+	});
+
+};
+
+module.exports.confirmUser = (event, context, callback) => {
+	const eventBodyJson = JSON.parse(event.body);
+
+	var params = {
+		ClientId: process.env.AWS_APP_CLIENT_ID,
+		ConfirmationCode: eventBodyJson.confirmationCode,
+		Username: eventBodyJson.emailAddress
+	};
+
+	cognitoIdentityServiceProvider.confirmSignUp(params, function(err, data) {
 		if( err ) {
 			console.error(err, err.stack);
 			return;
